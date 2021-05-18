661863e040d717e54dfb14518213b20e1560e4d2
Aaron van den Berg <a.vdberg98@gmail.com>
2021-05-18 13:47:08 +0200

    Added Patients API

---
+++ PoC/patients-API/Models/Patient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace patients_API.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int TelephoneNumber { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Gender { get; set; }
+        public string BloodType { get; set; }
+    }
+}
