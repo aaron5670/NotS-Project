27c3420f607be506e1f8e0182f7f5dfc6cdcbca3
Aaron van den Berg <a.vdberg98@gmail.com>
2021-05-25 14:40:41 +0200

    Added TensorFlow support + updated face-API dependency

--- poc/api/faceapi/faceapi/app.js
+++ poc/api/faceapi/faceapi/app.js
@@ -1,13 +1,13 @@
-const debug = require('debug')('my express app');
-const faceapi = require('face-api.js');
+const tf = require('@tensorflow/tfjs-node')
+const faceapi = require('@vladmandic/face-api');
 const canvas = require('canvas');
 const express = require('express');
-const logger = require('morgan');
 const bodyParser = require('body-parser');
 const routes = require('./routes/index');
 const users = require('./routes/users');
 
 const app = express();
+app.use(bodyParser.json({ limit: "10mb" }));
 
 const { Canvas, Image, ImageData } = canvas;
 faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
@@ -21,15 +21,10 @@ Promise.all([
     faceapi.nets.ssdMobilenetv1.loadFromDisk('./models')
 ]).then(() => setupFaceapi())
 
-app.use(logger('dev'));
-app.use(bodyParser.json({ limit: "50mb" }));
 
 app.post('/image', async (req, res) => {
     const {base64} = req.body;
-
     const i = await canvas.loadImage(`data:image/png;base64,${base64}`);
-
-    console.log('image uploaded')
     const displaySize = { width: i.width, height: i.height }
     const detections = await faceapi.detectAllFaces(i).withFaceLandmarks().withFaceDescriptors()
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
@@ -38,68 +33,29 @@ app.post('/image', async (req, res) => {
     return res.send(results);
 });
 
-const makeImage = async (image) => {
-    return await canvas.loadImage(image)
-}
-
 
 app.use('/', routes);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
 app.set('port', process.env.PORT || 3000);
 
-var server = app.listen(app.get('port'), function () {
-    debug('Express server listening on port ' + server.address().port);
+const server = app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + server.address().port);
 });
 
 const setupFaceapi = async () => {
     desc = await loadLabeledImages();
     faceMatcher = new faceapi.FaceMatcher(desc, 0.6)
-    console.log(desc);
-    console.log('$$$$$$$$$$$ Laden images met labels compleet')
+    console.log('Images data set loaded')
 }
 
 function loadLabeledImages() {
-    const labels = ['Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark']
+    const labels = ['Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Mark Rutte', 'Tony Stark']
     return Promise.all(
         labels.map(async label => {
-            console.log(label)
             const descriptions = []
             for (let i = 1; i <= 2; i++) {
-                //const img = await faceapi.fetchImage(`${API_URL}${label}/${i}.jpg`)
                 const img = await canvas.loadImage(`./images/${label}/${i}.jpg`)
-                console.log(img)
                 const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
                 descriptions.push(detections.descriptor)
             }
@@ -108,3 +64,4 @@ function loadLabeledImages() {
         })
     )
 }
+
